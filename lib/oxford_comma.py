970b64d6e76bb66dbf4e26caa87f1ff0865b35a3
Matthew Bomett <bomettmat2@gmail.com>
2023-11-29 20:26:54 +0300

    Completed Assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
+import string
+
 def oxford_comma(items):
-    return None
+    # joins a list of strings
+    if len(items) <= 1:
+    # returns the first item, otherwise returns an empty string
+        return items[0]
+    # returns two items with and between them
+    if len(items) == 2:
+        last_item = f' and {items[-1]}'
+    # otherwise returns the last item with and before it without a comma
+    else:
+        last_item = f', and {items[-1]}'
+    # constructs the final string
+    return f', '.join(items[:-1])  + last_item
